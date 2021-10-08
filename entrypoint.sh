cacfdce8d09fc316f3408260e258ed8754d0eddb
AsbjornOlling <asbjornolling@gmail.com>
2021-10-08 12:03:26 +0200

    only download sbdb if db isn't populated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-SBDBFILE=./data/pipeline/static/latest_sbdb.csv 
-if [ ! -f $SBDBFILE ]; then
-  echo "Downloading SBDB csv"
-  wget 'https://www.ianww.com/latest_fulldb.csv' -O $SBDBFILE
-fi
-
 # count entries in mongo, to determine whether a scrape is needed
 COUNT=$(python3 -c "import pymongo; print(pymongo.MongoClient('$MONGODB_CONNECTION_STRING').asterank.asteroids.estimated_document_count())")
 echo "Found $COUNT entries in mongo."
 
 if [ $COUNT -lt 700000 ]; then
+  SBDBFILE=./data/pipeline/static/latest_sbdb.csv
+  if [ ! -f $SBDBFILE ]; then
+    echo "Downloading SBDB csv"
+    wget 'https://www.ianww.com/latest_fulldb.csv' -O $SBDBFILE
+  fi
+
   echo "Running data pipeline..."
   ./data/pipeline/pipeline
 fi
